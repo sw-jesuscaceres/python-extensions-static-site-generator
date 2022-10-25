2e70e672398f6621849af3f38c61d4c29b70eba9
Jesus Caceres <jesus.caceres@southworks.com>
2022-10-25 12:48:27 -0300

    Fix

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -6,10 +6,9 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
-
     for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
-            if parser.valid_file_ext(parser.suffix):
+            if parser.valid_file_ext(path.suffix):
                 files.append(path)
 
 
@@ -17,6 +16,5 @@ def collect_files(source, site_parsers):
 def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext: template.format(name, ext, name.title())
-
-    menu = "\n".join([menu_item(path.steam, ext) for path in files])
-    return "<ul>\n{}<ul>\n{}".format(menu, html)
+    menu = "\n".join([menu_item(path.stem, ext) for path in files])
+    return "<ul>\n{}</ul>\n{}".format(menu, html)
