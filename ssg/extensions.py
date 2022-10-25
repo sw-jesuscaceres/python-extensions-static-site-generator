ea742ee2785334311dcd69596d6b2585dae3b572
Jesus Caceres <jesus.caceres@southworks.com>
2022-10-25 12:22:34 -0300

    Fix v1

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -2,14 +2,17 @@ import sys
 import importlib
 from pathlib import Path
 
+
 def load_module(directory, name):
-    sys.path.insert(directory, 0)
+    sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
 
+
 def load_directory(directory: Path):
-    for path in directory.rglob(".py"):
+    for path in directory.rglob("*.py"):
         load_module(directory.as_posix(), path.stem)
+
 
 def load_bundled():
     directory = Path(__file__).parent() / "extensions"
