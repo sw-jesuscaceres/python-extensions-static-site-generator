bd7e2544c54fa26898e7ac21f46341dacc30d594
Jesus Caceres <jesus.caceres@southworks.com>
2022-10-25 12:44:16 -0300

    Menu Extension

---
+++ ssg/extensions/menu.py
@@ -0,0 +1,22 @@
+from ssg import hooks, parsers
+
+files = []
+
+
+@hooks.register("collect_files")
+def collect_files(source, site_parsers):
+    def valid(p): return not isinstance(p, parsers.ResourceParser)
+
+    for path in source.rglob("*"):
+        for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext(parser.suffix):
+                files.append(path)
+
+
+@hooks.register("generate_menu")
+def generate_menu(html, ext):
+    template = '<li><a href="{}{}">{}</a></li>'
+    def menu_item(name: str, ext): template.format(name, ext, name.title())
+
+    menu = "\n".join([menu_item(path.steam, ext) for path in files])
+    return "<ul>\n{}<ul>\n{}".format(menu, html)
